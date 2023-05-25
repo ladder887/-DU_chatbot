e6e67614910d54dbd5ca46363d8736678c22eabf
pgm0506 <129020663+pgm0506@users.noreply.github.com>
2023-05-26 02:11:39 +0900

    Update UI_test.py
    버튼 추가 및 수정

--- UI_test.py
+++ UI_test.py
@@ -17,18 +17,39 @@ def message():
   if content == u"홈버튼":
     response = {
       "message" : {
-        "text" : "도움이 필요한 메뉴를 선택해주십시오"
+        "text" : "도움이 필요한 메뉴를 선택해 주십시오"
       },
       "keyborad" : {
         "type" : "buttons",
-        "buttons" : ["교직원 식당 메뉴", "학과공지", "오늘의 점심 추천","홈버튼"]
+        "buttons" : ["복지관 식당 메뉴", "학과공지", "오늘의 점심 추천", "홈버튼"]
       }
     }
     
-   elif content == u"교직원 식당 메뉴":
+  elif content == u"복지관 식당 메뉴":
     response = {
       "message" : {
-        "text" : "오늘 교직원 식당의 메뉴입니다."                                    #교직원 식당메뉴 추출 필요
+        "text" : "복지관 식당의 메뉴입니다."                                    #메뉴를 무슨 자료형이든 우리가 저장 or 긁어오기(텍스트, 텍스트 이미지)*메뉴판 사진은 별로일 듯
+        
+      },
+      "keyboard" : {
+        "type" : "buttons",
+        "buttons" : ["오늘의 점심 추천", "홈버튼"]
+      }
+    }
+  elif content == u"학과 공지":
+    response == {
+      "message" : {
+        "text" : "학과 공지입니다."                                            #학과 공지를 제목만 가져와서 선택하면 링크할 수 있나?
+      },                                                                      #massage_button(공지글로 가는 url), photo(공지 사진)
+      "keyboard" : {
+        "type" : "buttons",
+        "buttons" : ["홈버튼"]
+      }
+    }
+  elif content == u"오늘의 점심 추천":
+    response == {
+      "message" : {
+        "text" : ""                                                          #랜덤 함수사용?       
       },
       "keyboard" : {
         "type" : "buttons",
@@ -36,11 +57,6 @@ def message():
       }
     }
     
-    elif content == u"학과 공지":
-      response == {
-        "message" : {
-          "text" : "학과 공지입니다."                                                #학과 공지를 제목만 가져와서 선택하면 링크할 수 있나?
-        }                                                                           #massage_button이나 photo로?
-      }
+  
       return jsonify(response)
         
