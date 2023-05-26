c986a519d3ab938b7338f3a15d4844489863a04a
이현우 <110169068+ladder887@users.noreply.github.com>
2023-05-27 05:02:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from flask import Flask, json, request, jsonify
 import sys
-
-from card import *  # 카카오톡 데이터 전송 양식
-from test import *
-
+from cafeteria import *
+from method import *
 
 app = Flask(__name__)
 
-@app.route("/index", methods=['POST'])
-def index():
-    response = test_index()
-    return response
+@app.route("/haksic", methods=['POST'])
+def haksic():
+    response = cafeteria_parser()
+    return jsonify(response)
 
-@app.route("/new", methods=['POST'])
-def new():
-    response = test_new()
-    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
