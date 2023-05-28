847dc484b75074a6c06b86ca0d4f98d5f3a35206
pgm0506 <129020663+pgm0506@users.noreply.github.com>
2023-05-28 23:46:57 +0900

    수정
    버튼 추가 및 오류 수정

--- haksa.py
+++ haksa.py
@@ -3,13 +3,22 @@ from bs4 import BeautifulSoup
 import time
 from method import *
 
-def haksa_parser():
+def haksa_parser(content):
+
+    if any(content['action']['detailParams']) == False:
+        date = int(time.strftime('%m', time.localtime(time.time())))
+
+    else:
+        content = content['action']['detailParams']['sys_date']["value"]
+        content = ''.join(str(e) for e in content)
+        content = content.replace(" ", "")
+        #print(content)
+
+
+
     url = "https://daegu.ac.kr/schedule/detail?schedule_info_seq=1"
     response = requests.get(url)
     response.raise_for_status()
-
-    date = time.strftime('%m', time.localtime(time.time()))
-    #print(date)
 
     soup = BeautifulSoup(response.text, "lxml")
     text = soup.find_all(attrs={'class':'left'})
@@ -20,14 +29,14 @@ def haksa_parser():
         if i % 2 == 0:
             text = text.get_text()
             num1 = text.strip()
-            month = num1[0:2]
+            month = int(num1[0:2])
             #print(month)
         else:
             text = text.get_text()
             text = text.strip()
             num2 = text.replace("대학", "")
 
-            if date == month:
+            if date == month or date == 13:
                 schedule.append(num1 + " : " + num2)
                 #print(schedule)
             elif date < month:
@@ -35,15 +44,15 @@ def haksa_parser():
             #print(i)
 
         i += 1
-
-    title = '이번달 학사일정'
+    
+    if date == 13:
+        title = "올해 학사일정"
+    else:
+        title = '{}월 학사일정'.format(date)
     description = schedule
     description = '\n'.join(str(e) for e in description)
     response = insert_card(title,description)
-    response = insert_button_text(response,"전체일정","학사 전체일정")
-    reply = make_reply('다음달 학사일정', '다음달 학사일정')
-    response = insert_replies(response, reply)
-    print(response)
-    return response
+    response = insert_button_text(response,"전체일정","올해학사일정")
 
-haksa_parser()
+    #print(response)
+    return response
