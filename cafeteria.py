c06d30ed47bcbc46988a0982b6ed78f964dd5911
pey9797 <129020629+pey9797@users.noreply.github.com>
2023-05-29 21:13:50 +0900

    코드수정.py

--- cafeteria.py
+++ cafeteria.py
@@ -11,9 +11,7 @@ headers = {
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
 }
 
-#당일 메뉴
 def today_menu(place):
-
     if place == "복지관":
         num = 2
         title = "복지관2F 교직원 오늘의 식단"
@@ -21,7 +19,6 @@ def today_menu(place):
         num = 3
         title = "웅지관1F 한식뷔페 오늘의 식단"
 
-    #요일 구분
     today = date.today()
     weekday_number = today.weekday()  # 월요일 0 일요일 6
 
@@ -32,22 +29,19 @@ def today_menu(place):
 
     date1 = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
-    #대구대 서버에 학식 데이터 요청
-    response = {
+    send = {
         'restaurant_seq': num,
         'menu_date': date1
     }
 
-    responses = requests.post('https://daegu.ac.kr/restaurant/menu/list', headers = headers, data=response)
-    res = responses.json()
-
+    data1 = requests.post('https://daegu.ac.kr/restaurant/menu/list', headers = headers, data=send)
+    res = data1.json()
 
     menu_total = []
     temp = res[weekday_number-1]
     #print(temp)
     temp = temp['menu_content']
 
-    #데이터 전처리
     hangul = re.compile('[^ ㄱ-ㅣ가-힣+]+') # 한글과 띄어쓰기를 제외한 모든 글자
     result = hangul.sub('', temp) # 한글과 띄어쓰기를 제외한 모든 부분을 제거
     result = result.split()
@@ -63,11 +57,10 @@ def today_menu(place):
     menu = '\n'.join(str(e) for e in menu_total)
     response = insert_card(title , menu)
     response = insert_button_text(response, "이번주 식단", "이번주 {}식단".format(place))
-    print(response)
+    #print(response)
 
     return response
 
-#일주일 메뉴
 def week_menu(place):
 
     if place == "복지관":
@@ -87,13 +80,14 @@ def week_menu(place):
 
     date1 = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
-    response = {
+    send = {
         'restaurant_seq': num,
         'menu_date': date1
     }
 
-    responses = requests.post('https://daegu.ac.kr/restaurant/menu/list', headers = headers, data=response)
-    res = responses.json()
+    data1 = requests.post('https://daegu.ac.kr/restaurant/menu/list', headers = headers, data=send)
+    res = data1.json()
+
     dateDict = {0: '월요일', 1: '화요일', 2: '수요일', 3: '목요일', 4: '금요일', 5: '토요일', 6: '일요일'}
 
     response = {'version': '2.0', 'template': {
@@ -122,12 +116,26 @@ def week_menu(place):
         #print(response)
     return response
 
-#메인함수
 def cafeteria_parser(content):
+    if (content['action']['detailParams'].get('place_cafeteria')) == None:
+        response  = insert_text("복지관 또는 웅지관을 선택해주세요")
+        reply = make_reply('복지관', '복지관')
+        response = insert_replies(response, reply)
+        reply = make_reply('웅지관', '웅지관')
+        response = insert_replies(response, reply)
+        return response
+
     place = content['action']['detailParams']['place_cafeteria']["value"]
     place = ''.join(str(e) for e in place)
     place = place.replace(" ", "")
-    print(place)
+    #print(place)
+    if ["복지관", "웅지관"] not in place:
+        response  = insert_text("복지관 또는 웅지관을 선택해주세요")
+        reply = make_reply('복지관', '복지관')
+        response = insert_replies(response, reply)
+        reply = make_reply('웅지관', '웅지관')
+        response = insert_replies(response, reply)
+        return response
 
     if (content['action']['detailParams'].get('sys_date')) == None:
         response  = today_menu(place)
@@ -137,8 +145,8 @@ def cafeteria_parser(content):
         date = date.replace(" ", "")
         if date == '이번주':
             response = week_menu(place)
-            print(response)
+            #print(response)
         else:
             response  = today_menu(place)
-            print(response)
+            #print(response)
     return response 
